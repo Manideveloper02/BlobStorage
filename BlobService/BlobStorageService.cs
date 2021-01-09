419218cc100df77332c3cae655a2a44ff080b4c3
Manideveloper02 <64354799+Manideveloper02@users.noreply.github.com>
2021-01-09 17:24:09 +0530

    Commit initial BlobService

---
+++ BlobService/BlobStorageService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace BlobStorage
+{
+    public class BlobStorageService
+    {
+        string _accessKey = string.Empty;
+        public BlobStorageService(string accessKey)
+        {
+            this._accessKey = accessKey;
+        }
+        public string UploadFileToBlob(string strFileName, byte[] fileData, string fileMimeType)
+        {
+            try
+            {
+                var _task = Task.Run(() => this.UploadFileToBlobAsync(strFileName, fileData, fileMimeType));
+                _task.Wait();
+                string fileUrl = _task.Result;
+                return fileUrl;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        private async Task<string> UploadFileToBlobAsync(string strFileName, byte[] fileData, string fileMimeType)
+        {
+            try
+            {
+                CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(_accessKey);
+                CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+                string strContainerName = "uploads";
+                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(strContainerName);
+                string fileName = DateTime.Now.ToUniversalTime().ToString("yyyy-MM-dd") + "/" + strFileName;
+                if (await cloudBlobContainer.CreateIfNotExistsAsync())
+                {
+                    await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+                }
+                if (fileName != null && fileData != null)
+                {
+                    CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(fileName);
+                    cloudBlockBlob.Properties.ContentType = fileMimeType;
+                    await cloudBlockBlob.UploadFromByteArrayAsync(fileData, 0, fileData.Length);
+                    return cloudBlockBlob.Uri.AbsoluteUri;
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using Azure.Storage.Blobs;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlobStorage.Controllers
+{
+    public class HomeController : Controller
+    { 
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(List<IFormFile> postedFiles)
+        {
+            const string connString = "DefaultEndpointsProtocol=https;AccountName=cosmodbfileupload;AccountKey=VBolHbSAJ1JBdg7cUjadGGArFv0itgX8YwK2dHu721dGNtGbBfMgvzr3KxA0FUGCQQxbOo75OlAfh4+DIUfHmg==;EndpointSuffix=core.windows.net";
+            BlobStorageService blobStorageService = new BlobStorageService(connString);
+            foreach (IFormFile postedFile in postedFiles)
+            {
+                byte[] fileData;
+                using (var target = new MemoryStream())
+                {
+                    postedFile.CopyTo(target);
+                    fileData = target.ToArray();
+                }
+            string fileStorageUrl = blobStorageService.UploadFileToBlob(postedFile.FileName, fileData, postedFile.ContentType);
+            }
+            return View();
+        }
+         
+    }
+}
